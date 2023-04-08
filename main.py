671945f0345ee3e5977a1396d7bbb79e0e6e9294
calebyhan <59373603+calebyhan@users.noreply.github.com>
2023-04-08 18:31:20 -0400

    commit

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import matplotlib.pyplot as plt
 
 import population
 
-population = population.Population(1000, 100, 10, 5, 25, 1)
+population = population.Population(1000, 1000, 10, 5, 15, 5)
 population.start()
 stats = population.get_stats()
 
 print(stats)
+
+plt.plot(list(range(0, 1000)), stats["population"])
+plt.xlabel("Year")
+plt.ylabel("Population")
+plt.show()
